21046f436fe802dffb4e9c07b9b97092c882ffc4
Elean Quintero <167714130+EleanQuintero@users.noreply.github.com>
2024-08-17 23:46:53 +0200

    cambios en los endpoints

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { CreateTodo } from './CreateTodo'
-
 export const Header: React.FC = () => {
   const imgURL = 'https://imgs.search.brave.com/YF8pWGiJn3akjHyxMBrdi2rTt2PfYyC8zjHnQtni-8s/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMtcHJvZHVjdGlv/bi5ucG1qcy5jb20v/MjU1YTExOGY1NmY1/MzQ2Yjk3ZTU2MzI1/YTEyMTdhMTYuc3Zn'
   const style = { width: '60px', heigh: 'auto' }
